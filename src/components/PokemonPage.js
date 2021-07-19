d0b559fa6d973d4fdecd7626c80e29e68f4d5cbe
Rob <robert.gentile@powerhrg.com>
2021-07-19 14:50:57 -0400

    Completed Assignment

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([])
+  const [pokemonSearch, setPokemonSearch] = useState("")
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+      .then((resp) => resp.json())
+      .then(setPokemon)
+  }, [])
+
+  function handleAddPokemon(addNewPokemon) {
+    setPokemon([...pokemon, addNewPokemon])
+  }
+
+  const pokemonRendered = pokemon.filter((poke) => 
+    poke.name.toLowerCase().includes(pokemonSearch.toLowerCase())
+  )
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm 
+        onAddPokemon={handleAddPokemon}
+      />
       <br />
-      <Search />
+      <Search 
+        pokemonSearch={pokemonSearch}
+        onChangePokemonSearch={setPokemonSearch}
+      />
       <br />
-      <PokemonCollection />
+      <PokemonCollection 
+        pokemon={pokemonRendered}
+      />
     </Container>
   );
 }
